528df106622987b601c07ebca62e0bde61e67005
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2024-07-10 15:46:24 +1000

    Dev (#4)
    * Merged branch 'cmake' into 'dev'
    
    Squashed commit of the following:
    
    commit 6a5e5bd4c26ff4d9c21044159b6b91eaa81f5579
    Author: Matt Wilson <matthew@synesis.com.au>
    Date:   Tue Feb 6 14:13:18 2024 +1100
    
        + CMake support
    
    * Merged 'mwsis/cmake-windows' into 'dev'
    
    Squashed commit of the following:
    
    commit f6a891ab8de1029c9a60cc0e7a0a5b60419d512f
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 15:26:18 2024 +1000
    
        ~ GCC warnings
    
    commit 2c4356652ea7f29746c575b320b973c41d8ff60b
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 15:15:57 2024 +1000
    
        ~ CMake project dependencies
    
    commit 24cb1048b26cdb2ba5463200d378914b03b7e2ac
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 15:15:37 2024 +1000
    
        ~ implicit-link file now compatible with VC++ 17
    
    commit 54317fed64b4b25e027d422fbf5557cfd87bd48b
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 15:14:50 2024 +1000
    
        ~ boilerplate files enhanced functionality
    
    commit 4c8e0a657675b385d178d61f386442b996e56638
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 15:06:46 2024 +1000
    
        ~ Windows CMake compatibility
    
    commit fa617f1970028b142008b79d1b2f41e9ff32cd6e
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 14:48:42 2024 +1000
    
        ~ boilerplate canonicalisation
    
    commit 3532fbeb003fdb63b14a1b310c9edf0e54d66fd8
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 14:47:02 2024 +1000
    
        ~ settings
    
    commit 77fd27587141e824bf54f27b23e1687e8f06b81a
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Wed Jul 10 14:44:24 2024 +1000
    
        ~ boilerplate canonicalisation
    
    ---------
    
    Co-authored-by: Matt Wilson <matthew@synesis.com.au>

--- include/b64/b64.h
+++ include/b64/b64.h
@@ -1,17 +1,17 @@
 /* /////////////////////////////////////////////////////////////////////////
- * File:        b64/b64.h
+ * File:    b64/b64.h
  *
- * Purpose:     Header file for the b64 library
+ * Purpose: Header file for the b64 library
  *
- * Created:     18th October 2004
- * Updated:     23rd December 2023
+ * Created: 18th October 2004
+ * Updated: 10th July 2024
  *
- * Thanks:      To Adam McLaurin, for ideas regarding the b64_decode2() and
- *              b64_encode2().
+ * Thanks:  To Adam McLaurin, for ideas regarding the b64_decode2() and
+ *          b64_encode2().
  *
- * Home:        https://github.com/synesissoftware/b64
+ * Home:    https://github.com/synesissoftware/b64
  *
- * Copyright (c) 2019-2023, Matthew Wilson and Synesis Information Systems
+ * Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2004-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -51,6 +51,7 @@
 #ifndef B64_INCL_B64_H_B64
 #define B64_INCL_B64_H_B64
 
+
 /* /////////////////////////////////////////////////////////////////////////
  * version information
  */
@@ -58,8 +59,8 @@
 #ifndef B64_DOCUMENTATION_SKIP_SECTION
 # define B64_VER_B64_H_B64_MAJOR    1
 # define B64_VER_B64_H_B64_MINOR    6
-# define B64_VER_B64_H_B64_REVISION 7
-# define B64_VER_B64_H_B64_EDIT     42
+# define B64_VER_B64_H_B64_REVISION 8
+# define B64_VER_B64_H_B64_EDIT     44
 #endif /* !B64_DOCUMENTATION_SKIP_SECTION */
 
 /** \def B64_VER_MAJOR
@@ -102,18 +103,22 @@
 # define B64_VER_1_4_4          0x010404ff
 # define B64_VER_1_4_5          0x010405ff
 # define B64_VER_1_4_6          0x010406ff
+# define B64_VER_1_4_7          0x010407ff
+# define B64_VER_1_4_8          0x010408ff
 #endif /* !B64_DOCUMENTATION_SKIP_SECTION */
 
 #define B64_VER_MAJOR       1
 #define B64_VER_MINOR       4
-#define B64_VER_REVISION    6
-#define B64_VER             0x010406ff
+#define B64_VER_REVISION    8
+#define B64_VER             0x010408ff
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * includes
  */
 
 #include <stddef.h>
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * features
@@ -128,6 +133,7 @@
 #  define B64_VARIANT_TEST
 # endif /* !B64_VARIANT_TEST */
 #endif /* SYNESIS_VARIANT_TEST */
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * namespace
@@ -150,16 +156,20 @@
 #ifndef B64_NO_NAMESPACE
 
 # ifdef B64_CUSTOM_NAMESPACE
-#  define B64_NAMESPACE     B64_CUSTOM_NAMESPACE
+
+#  define B64_NAMESPACE                                     B64_CUSTOM_NAMESPACE
 # else /* ? B64_CUSTOM_NAMESPACE */
-#  define B64_NAMESPACE     b64
+
+#  define B64_NAMESPACE                                     b64
 # endif /* B64_CUSTOM_NAMESPACE */
 
 # if defined(B64_CUSTOM_NAMESPACE) && \
      defined(B64_CUSTOM_NAMESPACE_QUALIFIER)
-#  define B64_NAMESPACE_QUALIFIER       B64_CUSTOM_NAMESPACE_QUALIFIER
+
+#  define B64_NAMESPACE_QUALIFIER                           B64_CUSTOM_NAMESPACE_QUALIFIER
 # else /* B64_CUSTOM_NAMESPACE && B64_CUSTOM_NAMESPACE_QUALIFIER */
-#  define B64_NAMESPACE_QUALIFIER       ::B64_NAMESPACE
+
+#  define B64_NAMESPACE_QUALIFIER                           ::B64_NAMESPACE
 # endif /* B64_CUSTOM_NAMESPACE && B64_CUSTOM_NAMESPACE_QUALIFIER */
 
 
@@ -174,6 +184,7 @@
 namespace B64_NAMESPACE
 {
 #endif /* !B64_NO_NAMESPACE */
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * typedefs
@@ -220,6 +231,7 @@ enum B64_FLAGS
 #ifndef __cplusplus
 typedef enum B64_FLAGS  B64_FLAGS;
 #endif /* !__cplusplus */
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * functions
@@ -431,7 +443,6 @@ b64_getErrorStringLength(
     B64_RC  code
 );
 
-
 /** \def b64_getStatusCodeString(code)
  *
  * Returns the textual description of the error
@@ -440,7 +451,7 @@ b64_getErrorStringLength(
  *
  * \see b64_getStatusCodeStringLength, b64_getErrorString, b64_getErrorStringLength
  */
-#define b64_getStatusCodeString         b64_getErrorString
+#define b64_getStatusCodeString                             b64_getErrorString
 
 /** \def b64_getStatusCodeStringLength(code)
  *
@@ -450,12 +461,12 @@ b64_getErrorStringLength(
  *
  * \see b64_getStatusCodeString, b64_getErrorString, b64_getErrorStringLength
  */
-#define b64_getStatusCodeStringLength   b64_getErrorStringLength
-
+#define b64_getStatusCodeStringLength                       b64_getErrorStringLength
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif /* __cplusplus */
+
 
 /* /////////////////////////////////////////////////////////////////////////
  * namespace
@@ -525,6 +536,7 @@ namespace stlsoft
 # endif /* !B64_DOCUMENTATION_SKIP_SECTION */
 
 #endif /* !B64_NO_NAMESPACE */
+
 
 /* ////////////////////////////////////////////////////////////////////// */
 
